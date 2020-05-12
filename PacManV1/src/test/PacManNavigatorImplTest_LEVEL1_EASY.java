3adbc73c40f49865e7397eb1d38833a027a4b737
Jeffrey Ng <komoto415@gmail.com>
2020-05-11 23:05:03 -0500

    starting tests over

--- PacManV1/src/test/PacManNavigatorImplTest_LEVEL1_EASY.java
+++ PacManV1/src/test/PacManNavigatorImplTest_LEVEL1_EASY.java
@@ -2,51 +2,40 @@ package test;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 import pacman.Direction;
 import pacman.PacManNavigator;
-import pacman.PacManNavigatorImpl_Ng;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import pacman.PacManNavigatorImpl_Skeleton;
 
 
 public class PacManNavigatorImplTest_LEVEL1_EASY {
     public final Direction N = Direction.N, E = Direction.E, S = Direction.S, W = Direction.W;
 
-    private String composite(List<String> rows) {
-        StringBuilder sb = new StringBuilder();
-        for (String row : rows) {
-            sb.append(row).append('\n');
-        }
-        return sb.toString().trim();
-    }
+    protected char[][] buildMazeSimpleNoCherries() {
+        String row01 = "|===================|";
+        String row02 = "|*******************|";
+        String row03 = "|*|*|==|*|=|*|==|*|*|";
+        String row04 = "|*|*|==|*|=|*|==|*|*|";
+        String row05 = "|*******************|";
+        String row06 = "|*|=|*|*|=O=|*|*|=|*|";
+        String row07 = "|*|***|***|***|*****|";
+        String row08 = "|*|===|=|*|*|=|===|*|";
+        String row09 = "|*******|*|*|*******|";
+        String row10 = "|*|====|**|**|====|*|";
+        String row11 = "|*|******|=|******|*|";
+        String row12 = "|***|==|*****|==|***|";
+        String row13 = "|===================|";
+        char[][] maze = new char[][]{
+                row01.toCharArray(), row02.toCharArray(), row03.toCharArray(), row04.toCharArray(), row05.toCharArray(),
+                row06.toCharArray(), row07.toCharArray(), row08.toCharArray(), row09.toCharArray(), row10.toCharArray(),
+                row11.toCharArray(), row12.toCharArray(), row13.toCharArray()
+        };
 
-    protected String buildMazeSimpleNoCherries() {
-        String row01 = "|-------------------------------------|";
-        String row02 = "|* * * * * * * * * * * * * * * * * * *|";
-        String row03 = "|*|---|*|-----|*|-----|*|---|*|-----|*|";
-        String row04 = "|*|---|*|-----|*|-----|*|---|*|-----|*|";
-        String row05 = "|* * * * * * * * * O * * * * * * * * *|";
-        String row06 = "|*|-----|*|-|*|---------|*|-|*|-----|*|";
-        String row07 = "|* * * * *|-|* * *| |* * *|-|* * * * *|";
-        String row08 = "|-------|*|-----|*| |*|-----|*|-------|";
-        String row09 = "|* * * * * * *| |*|-|*| |* * * * * * *|";
-        String row10 = "|*|---------|*| |*|*|*| |*|---------|*|";
-        String row11 = "|*|-|* * * * *|-|*|-|*|-|* * * * *|-|*|";
-        String row12 = "|* * *|-----|* * *| |*|*|*|-----|* * *|";
-        String row13 = "|-------------------------------------|";
-        List<String> rows = new ArrayList<>(
-                Arrays.asList(row01, row02, row03, row04, row05, row06, row07, row08, row09, row10, row11, row12,
-                        row13));
 
-        return composite(rows);
+        return maze;
     }
 
     protected PacManNavigator getPacManSimpleNoCherries(int maxTurns) {
-        return new PacManNavigatorImpl_Ng(buildMazeSimpleNoCherries(), maxTurns);
+        return new PacManNavigatorImpl_Skeleton(buildMazeSimpleNoCherries(), maxTurns);
     }
 
     @Test
@@ -55,19 +44,19 @@ public class PacManNavigatorImplTest_LEVEL1_EASY {
         PacManNavigator navigator = getPacManSimpleNoCherries(maxTurns);
         System.out.println(navigator);
 
-//        assertEquals(10, navigator.maxPotentialPellets());
-//
-//        navigator.move(E);
-//        navigator.move(E);
-//        navigator.move(E);
-//        navigator.move(S);
-//        navigator.move(S);
-//        navigator.move(W);
-//        navigator.move(W);
-//
-//        assertEquals(3, navigator.movesLeft());
-//        assertFalse(navigator.noMorePelletsToGather());
-//        assertArrayEquals(new int[]{1, 1, 0, 0}, navigator.getEdges());
+        //        assertEquals(10, navigator.maxPotentialPellets());
+        //
+        //        navigator.move(E);
+        //        navigator.move(E);
+        //        navigator.move(E);
+        //        navigator.move(S);
+        //        navigator.move(S);
+        //        navigator.move(W);
+        //        navigator.move(W);
+        //
+        //        assertEquals(3, navigator.movesLeft());
+        //        assertFalse(navigator.noMorePelletsToGather());
+        //        assertArrayEquals(new int[]{1, 1, 0, 0}, navigator.getEdges());
 
     }
 
