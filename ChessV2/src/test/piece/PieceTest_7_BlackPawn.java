5b82934e8327a4569ab152698ab3c3a8b45a319e
Jeffrey Ng <komoto415@gmail.com>
2020-05-14 01:09:49 -0500

    added ternary condition to account for player based pawn tests

--- ChessV2/src/test/piece/PieceTest_7_BlackPawn.java
+++ ChessV2/src/test/piece/PieceTest_7_BlackPawn.java
@@ -1,9 +1,11 @@
 package test.piece;
 
 import chess.GridPosition;
+import chess.Player;
 import chess.piece.Piece;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -11,9 +13,26 @@ import static org.junit.Assert.assertArrayEquals;
 /**
  @author Jeffrey Ng
  @created 2020-05-13 */
-public class PieceTest_7_BlackPawn extends BlackPiece {
+public class PieceTest_7_BlackPawn extends PieceTest_6_WhitePawn {
+
+    @Override
+    protected Piece getPieceFromTestName(String testName) throws ClassNotFoundException, NoSuchMethodException,
+            IllegalAccessException, InvocationTargetException, InstantiationException {
+
+        String classNameAux = testName.length() < 10 ? testName.substring(0, testName.length() - 2) :
+                              testName.substring(5, testName.length() - 2);
+        char title = classNameAux.toUpperCase().charAt(0);
+        String restOfTheClassName = classNameAux.substring(1);
+        StringBuilder sb = new StringBuilder();
+        String className = sb.append(title).append(restOfTheClassName).toString();
+        Class<?> clazz = Class.forName(String.format("chess.piece.%s", className));
+        Constructor<?> constructor = clazz.getConstructor(Player.class);
+        return testName.length() < 10 ? (Piece) constructor.newInstance(Player.WHITE) :
+               (Piece) constructor.newInstance(Player.BLACK);
+    }
+
     @Test
-    public void pawnA7() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
+    public void blackPawnA7() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
             InstantiationException, IllegalAccessException {
         String testName = Thread.currentThread().getStackTrace()[1].getMethodName();
         GridPosition gp = extractGridPositionFromTestName(testName);
@@ -35,7 +54,7 @@ public class PieceTest_7_BlackPawn extends BlackPiece {
     }
 
     @Test
-    public void pawnA5() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
+    public void blackPawnA5() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
             InstantiationException, IllegalAccessException {
         String testName = Thread.currentThread().getStackTrace()[1].getMethodName();
         GridPosition gp = extractGridPositionFromTestName(testName);
@@ -57,7 +76,7 @@ public class PieceTest_7_BlackPawn extends BlackPiece {
     }
 
     @Test
-    public void pawnC1() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
+    public void blackPawnC1() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException,
             InstantiationException, IllegalAccessException {
         String testName = Thread.currentThread().getStackTrace()[1].getMethodName();
         GridPosition gp = extractGridPositionFromTestName(testName);
